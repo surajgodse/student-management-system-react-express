f60f374c4df49dbb24a33568add8b25d8b67419a
surajgodse <godsesuraj2140@gmail.com>
2024-08-27 22:04:21 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+const port = process.env.PORT || 9000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+/*const con = mysql.createConnection({
+	host:"sql12.freesqldatabase.com",
+	user:"sql12719954",
+	password:"6RxWGX8bWT",
+	database:"sql12719954"
+});*/
+const con = mysql.createConnection({
+	host:"localhost",
+	user:"root",
+	password:"Root@2140",
+	database:"sms15july24"
+});
+
+app.post("/save", (req, res)=>{
+	let data = [req.body.rno, req.body.name, req.body.marks];
+	let sql = "insert into student values(?,?, ?)";
+	con.query(sql, data, (err, result)=>{
+		if(err)		res.send(err);
+		else 		res.send(result);
+	});
+});
+
+app.get("/read", (req, res)=>{
+	let data = [req.body.rno, req.body.name, req.body.marks];
+	let sql = "select * from student";
+	con.query(sql, (err, result)=>{
+		if(err)		res.send(err);
+		else 		res.send(result);
+	});
+});
+
+
+app.delete("/remove", (req, res)=>{
+	let data = [req.body.rno];
+	let sql = "delete from student where rno=?";
+	con.query(sql, data, (err, result)=>{
+		if(err)		res.send(err);
+		else 		res.send(result);
+	});
+});
+
+
+app.put("/modify", (req, res)=>{
+	let data = [req.body.name, req.body.marks, req.body.rno];
+	let sql = "update student set name=?, marks=? where rno=?";
+	con.query(sql, data, (err, result)=>{
+		if(err)		res.send(err);
+		else 		res.send(result);
+	});
+});
+
+
+
+app.listen(port, () => { console.log("ready @ 9000"); });	
